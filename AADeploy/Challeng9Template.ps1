9c561f8a649fcbc8d100b2aac0b965bf7335f43a
twallace27603 <tswwci@hotmail.com>
2018-09-12 12:06:18 -0400

    Fixed the storage connection string in template

---
+++ AADeploy/Challeng9Template.ps1
@@ -0,0 +1,30 @@
+﻿$LabInstanceId = "7223858"
+
+param($LabInstanceId)
+
+$id = $($LabInstanceId)
+$rg = "Archlod$($id)"
+$saName = "sa$($id)"
+
+$sa = Get-AzureRmStorageAccount -ResourceGroupName $rg -Name $saName -ErrorAction Ignore
+$key = (Get-AzureRmStorageAccountKey -ResourceGroupName $rg -Name $saName -ErrorAction Ignore)[0].Value
+$context = New-AzureStorageContext -StorageAccountName $saName -StorageAccountKey $key
+
+$container = Get-AzureStorageContainer -Name `$logs -Context $context -ErrorAction Ignore
+$container -ne $null
+
+param($LabInstanceId)
+
+$id = $($LabInstanceId)
+$rg = "Archlod$($id)"
+$waName = "lods$($id)"
+$wa = Get-AzureRmWebApp -ResourceGroupName $rg -Name $waName
+
+$result=$false
+
+foreach($cs in $wa.SiteConfig.ConnectionStrings) {
+    if($cs.Name -eq "storageConnection") {
+        $result = $true
+    }
+}
+return $result
---
+++ AADeploy/Challenge3ActionTest.ps1
@@ -0,0 +1,27 @@
+﻿$LabInstanceId = "7202203"
+
+param($LabInstanceId)
+
+$id = $($LabInstanceId)
+$rg = "Archlod$($id)"
+$laName = "logic$($id)"
+
+$la = Get-AzureRmLogicApp -ResourceGroupName $rg -Name $laName -ErrorAction Ignore
+
+$result = $false
+#This is super clugey and will fail if MS changes the structure of a logic app at all.
+if($la -ne $null) {
+    foreach ($def in $la.Definition) {
+        foreach($lvl2 in $def) {
+            if($lvl2.Type -ne "String") {
+                foreach($lvl3 in $lvl2) {
+                    if($lvl3.Name -eq "Insert_Row") {
+                        $result = $true
+                    }
+                }
+            }
+        }
+    }
+}
+
+$result
